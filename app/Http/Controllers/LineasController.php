df237770ae37029f43ef43357796d9f515f371dd
diter14 <diter.terrones14@gmail.com>
2017-02-26 23:29:59 -0500

    Model Empresa

--- app/Http/Controllers/LineasController.php
+++ app/Http/Controllers/LineasController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Models\Empresa;
 
 class LineasController extends Controller
 {
 	public function index()
-	{
-		return view('lineas.index');
+	{	
+		$lineas = Empresa::all();
+		return view('lineas.index',compact('lineas'));
 	}
+
+	public function show(Bus $bus)
+	{
+		return view('lineas.show',compact('bus'));
+	}
+
 }
--- resources/views/lineas/index.blade.php
+++ resources/views/lineas/index.blade.php
@@ -3,5 +3,10 @@
 @section('content')
 	<div class="container fluid">
 		<h3>Líneas de Transporte</h3>
+		<ul>
+			@foreach ($lineas as $linea)
+				<li>{{ $linea->razon_social }}</li>
+			@endforeach
+		</ul>
 	</div>
 @stop
